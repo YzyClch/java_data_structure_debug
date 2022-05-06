e783dddb42daa50844713036cf5334fae682c06a
yzy <yezy@daishupei.com>
2022-05-06 21:33:19 +0800

    debug

---
+++ src/main/java/com/yzy/test/HashMapDebug.java
@@ -0,0 +1,17 @@
+package com.yzy.test;
+
+
+import com.yzy.util.HashMap;
+
+public class HashMapDebug {
+
+
+    public static void main(String[] args) {
+        HashMap<Object, Object> debugMap = new HashMap<>();
+        debugMap.put("0","");
+        debugMap.put("1","");
+        debugMap.put("2","");
+        debugMap.put("3","");
+    }
+
+}
