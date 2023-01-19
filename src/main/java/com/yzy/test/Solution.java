1d04e17b1fc7c62f5934b5085ccc876c2db9d819
yzy <1241614483@qq.com>
2023-01-20 01:32:09 +0800

    共有前缀

--- src/main/java/com/yzy/test/Solution.java
+++ src/main/java/com/yzy/test/Solution.java
@@ -61,6 +61,28 @@ public class Solution {
         return s;
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        //边界条件判断
+        if (strs == null || strs.length == 0)
+            return "";
+        //默认第一个字符串是他们的公共前缀
+        String pre = strs[0];
+        int i = 1;
+        while (i < strs.length) {
+            //不断的截取
+            while (strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0, pre.length() - 1);
+            i++;
+        }
+        return pre;
+    }
+
+    @Test
+    public void testlongestCommonPrefix(){
+        System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+
+
     public String describe(String s){
         char[] chars = s.toCharArray();
         char pre ='?';
