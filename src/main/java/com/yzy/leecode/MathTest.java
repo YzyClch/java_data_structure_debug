123e3fa08ebf56ac370395acaa90324a76813cb7
yzy <1241614483@qq.com>
2023-02-06 21:52:39 +0800

    罗马整数转数字

--- src/main/java/com/yzy/leecode/MathTest.java
+++ src/main/java/com/yzy/leecode/MathTest.java
@@ -26,6 +26,32 @@ public class MathTest {
         return r;
     }
 
+    public int romanToInt(String s) {
+
+
+        int pre = 0, cur;
+        int sum = 0;
+        int size = s.length();
+        for(int i = 0; i < size; i++) {
+            switch(s.charAt(i)) {
+                case 'I': cur = 1;    sum += cur; pre = cur; break;
+                case 'V': cur = 5;    sum += cur - (pre == 1 ? 2 : 0); pre = cur; break;
+                case 'X': cur = 10;   sum += cur - (pre == 1 ? 2 : 0); pre = cur; break;
+                case 'L': cur = 50;   sum += cur - (pre == 10 ? 20 : 0); pre = cur; break;
+                case 'C': cur = 100;  sum += cur - (pre == 10 ? 20 : 0); pre = cur; break;
+                case 'D': cur = 500;  sum += cur - (pre == 100 ? 200 : 0); pre = cur; break;
+                case 'M': cur = 1000; sum += cur - (pre == 100 ? 200 : 0); pre = cur; break;
+                default: break;
+            }
+        }
+        return sum;
+    }
+
+    @Test
+    public void testromanToInt(){
+        System.out.println(romanToInt("VI"));
+    }
+
     public int countPrimes(int n) {
         boolean[] arr = new boolean[n];
         int c=0;
